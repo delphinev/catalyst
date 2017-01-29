261c79955ce58625021b4f9530aa179a4bd1c747
Delphine Vilaysack <delphine.vilaysack@gmail.com>
2017-01-29 17:12:47 +0100

    Add biography and edit profile

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,8 +1,19 @@
 class RegistrationsController < Devise::RegistrationsController
 
+  protected
+
+    def after_update_path_for(resource)
+      user_path(current_user)
+    end
+
   private
 
     def sign_up_params
-      params.require(:user).permit(:username, :first_name, :last_name, :email, :password, :password_confirmation)
+      params.require(:user).permit(:username, :first_name, :last_name, :email, :password, :password_confirmation, :biography)
     end
+
+    def account_update_params
+      params.require(:user).permit(:username, :first_name, :last_name, :email, :password, :password_confirmation, :current_password, :biography)
+    end
+
 end
